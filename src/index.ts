3ffb2651ef8784f1b97e2f05e4f0851d5a44d22f
Lazar Mitic <lazar@tempus.finance>
2023-05-08 14:44:44 +0200

    Export token types (#24)

--- src/index.ts
+++ src/index.ts
@@ -8,3 +8,5 @@ export { Position, PositionWithAddress, UserPosition } from './position';
 export { PriceFeed } from './price';
 export { Balance } from './balance';
 export { Stats } from './stats';
+export type { CollateralToken, RToken, Token, UnderlyingCollateralToken } from './types';
+export { COLLATERAL_TOKENS, R_TOKEN, TOKENS, UNDERLYING_COLLATERAL_TOKENS } from './types';
