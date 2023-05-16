3de5c839557769a19d751cf3503540d9557ff9a2
Vukašin Manojlović <iamvukasin@gmail.com>
2023-05-16 14:16:26 +0200

    Move permit to utils (#55)

--- src/constants.ts
+++ src/constants.ts
@@ -5,10 +5,6 @@ import { Decimal } from '@tempusfinance/decimal';
 export const MIN_COLLATERAL_RATIO = new Decimal(1.1); // 110%
 export const MIN_NET_DEBT = new Decimal(3000); // 3000 R
 
-export const PERMIT_DEADLINE_SHIFT = 30 * 60; // 30 minutes
-
-export const TOKENS_WITH_PERMIT = ['wstETH'];
-
 export const GAS_LIMIT_MULTIPLIER = new Decimal(1.2); // 20%
 
 export const SUBGRAPH_ENDPOINT_URL = 'https://api.studio.thegraph.com/proxy/46633/raft-test/0.1.0';
---
+++ src/utils/index.ts
@@ -0,0 +1,1 @@
+export { createEmptyPermitSignature, createPermitSignature } from './permit';
