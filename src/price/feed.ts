5e79843bd343029052bc896498896839b685fc07
Lazar <Lazar__93@hotmail.com>
2023-05-17 11:06:35 +0200

    set price

--- src/price/feed.ts
+++ src/price/feed.ts
@@ -1,17 +1,20 @@
 import { Decimal } from '@tempusfinance/decimal';
-import { Contract, Provider } from 'ethers';
+import { Contract, JsonRpcSigner, Provider } from 'ethers';
 import { RaftConfig } from '../config';
 import { PositionManager, PositionManager__factory, WstETH, WstETH__factory } from '../typechain';
 import { R_TOKEN, Token, UnderlyingCollateralToken } from '../types';
 
 export class PriceFeed {
   private provider: Provider;
+  private signer: JsonRpcSigner | null = null;
   private positionManager: PositionManager;
   private priceFeeds = new Map<UnderlyingCollateralToken, Contract>();
+  private setPriceFeeds = new Map<UnderlyingCollateralToken, Contract>();
   private collateralTokens = new Map<UnderlyingCollateralToken, WstETH>();
 
-  public constructor(provider: Provider) {
+  public constructor(provider: Provider, signer?: JsonRpcSigner) {
     this.provider = provider;
+    this.signer = signer || null;
     this.positionManager = PositionManager__factory.connect(RaftConfig.addresses.positionManager, provider);
   }
 
@@ -38,6 +41,12 @@ export class PriceFeed {
     }
   }
 
+  public async setPrice(price: Decimal): Promise<void> {
+    const feed = await this.loadSetPriceFeed('wstETH');
+
+    await feed.setPrice(price.toBigInt(Decimal.PRECISION));
+  }
+
   private async loadPriceFeed(token: UnderlyingCollateralToken): Promise<Contract> {
     if (!this.priceFeeds.has(token)) {
       const priceFeedAddress = await this.positionManager.priceFeeds(RaftConfig.getTokenAddress(token) as string);
@@ -48,6 +57,22 @@ export class PriceFeed {
     }
 
     return this.priceFeeds.get(token) as Contract;
+  }
+
+  private async loadSetPriceFeed(token: UnderlyingCollateralToken): Promise<Contract> {
+    if (!this.setPriceFeeds.has(token)) {
+      const priceFeedAddress = await this.positionManager.priceFeeds(RaftConfig.getTokenAddress(token) as string);
+      const contract = new Contract(
+        priceFeedAddress,
+        ['function setPrice(uint256 price) external returns (bool)'],
+        this.signer,
+      );
+
+      this.setPriceFeeds.set(token, contract);
+      return contract;
+    }
+
+    return this.setPriceFeeds.get(token) as Contract;
   }
 
   private async loadCollateralToken(): Promise<WstETH> {
