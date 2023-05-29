ad1b4a37757d29471f7f134fa340205843eaa0e3
Vukašin Manojlović <iamvukasin@gmail.com>
2023-05-29 13:18:45 +0200

    Extract gas limit setting (#75)

--- src/constants.ts
+++ src/constants.ts
@@ -5,6 +5,4 @@ import { Decimal } from '@tempusfinance/decimal';
 export const MIN_COLLATERAL_RATIO = new Decimal(1.1); // 110%
 export const MIN_NET_DEBT = new Decimal(3000); // 3000 R
 
-export const GAS_LIMIT_MULTIPLIER = new Decimal(1.2); // 20%
-
 export const SUBGRAPH_ENDPOINT_URL = 'https://api.studio.thegraph.com/proxy/46633/raft-test/0.1.1';
