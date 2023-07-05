ab11c5c1a25293b23f268ee1e9dcb2cbfcf3f4ce
Lazar <Lazar__93@hotmail.com>
2023-07-05 12:18:20 +0200

    Add open leverage position

--- src/constants.ts
+++ src/constants.ts
@@ -5,3 +5,5 @@ import { Decimal } from '@tempusfinance/decimal';
 export const MIN_COLLATERAL_RATIO = new Decimal(1.2); // 120%
 export const MIN_NET_DEBT = new Decimal(3000); // 3000 R
 export const SUBGRAPH_PRICE_PRECISION = 8;
+export const BALANCER_R_DAI_POOL_ID = '0x20a61b948e33879ce7f23e535cc7baa3bc66c5a9000000000000000000000555';
+export const DAI_TOKEN_ADDRESS = '0x6B175474E89094C44Da98b954EedeAC495271d0F';
