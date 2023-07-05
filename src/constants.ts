4120b4db360464cfb2b8390b9f5d8425592103af
Vukašin Manojlović <iamvukasin@gmail.com>
2023-07-05 10:36:58 +0200

    Add more unit tests (#141)

--- src/constants.ts
+++ src/constants.ts
@@ -1,7 +1,11 @@
 import { Decimal } from '@tempusfinance/decimal';
+import { UnderlyingCollateralToken } from './types';
 
 // Protocol constants
 
-export const MIN_COLLATERAL_RATIO = new Decimal(1.2); // 120%
+export const MIN_COLLATERAL_RATIO: Record<UnderlyingCollateralToken, Decimal> = {
+  wstETH: new Decimal(1.2), // 120%
+  wcrETH: new Decimal(1.2), // 120%
+};
 export const MIN_NET_DEBT = new Decimal(3000); // 3000 R
 export const SUBGRAPH_PRICE_PRECISION = 8;
