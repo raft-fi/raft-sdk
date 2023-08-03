54f3c8a26f908a4fbe336fe979cb503ad3ddaf71
Vukašin Manojlović <iamvukasin@gmail.com>
2023-08-03 10:59:13 +0200

    Make some tests CI only

--- tests/utils/transactions.test.ts
+++ tests/utils/transactions.test.ts
@@ -3,6 +3,7 @@ import { buildTransactionWithGasLimit, getTokenContract } from '../../src/utils'
 import { TypedContractMethod } from '../../src/typechain/common';
 import { JsonRpcProvider, Wallet } from 'ethers';
 import { RaftConfig } from '../../src/config';
+import { itWhenCI } from '../test-utils';
 
 const mockMethod = {
   estimateGas: jest.fn().mockResolvedValue(100000n),
@@ -22,49 +23,53 @@ describe('buildTransactionWithGasLimit', () => {
     expect(gasLimitNoSigner).toEqual(new Decimal(150000n, Decimal.PRECISION));
   });
 
-  it('should append tag to transaction data', async () => {
-    const provider = new JsonRpcProvider('http://127.0.0.1:8545');
-    const signer = Wallet.fromPhrase('test test test test test test test test test test test junk', provider);
-    const tag = 'tag';
-    const stETH = getTokenContract('R', signer);
+  itWhenCI(
+    'should append tag to transaction data',
+    async () => {
+      const provider = new JsonRpcProvider('http://127.0.0.1:8545');
+      const signer = Wallet.fromPhrase('test test test test test test test test test test test junk', provider);
+      const tag = 'tag';
+      const stETH = getTokenContract('R', signer);
 
-    const { sendTransaction: sendTransactionNoTag } = await buildTransactionWithGasLimit(
-      stETH.approve,
-      [RaftConfig.getPositionManagerAddress('wstETH'), 1n],
-      undefined,
-      undefined,
-      signer,
-    );
-    const transactionResponseNoTag = await sendTransactionNoTag();
-    await transactionResponseNoTag.wait();
+      const { sendTransaction: sendTransactionNoTag } = await buildTransactionWithGasLimit(
+        stETH.approve,
+        [RaftConfig.getPositionManagerAddress('wstETH'), 1n],
+        undefined,
+        undefined,
+        signer,
+      );
+      const transactionResponseNoTag = await sendTransactionNoTag();
+      await transactionResponseNoTag.wait();
 
-    const { sendTransaction: sendTransactionWithTag } = await buildTransactionWithGasLimit(
-      stETH.approve,
-      [RaftConfig.getPositionManagerAddress('wstETH'), 1n],
-      undefined,
-      tag,
-      signer,
-    );
-    const transactionResponseWithTag = await sendTransactionWithTag();
-    await transactionResponseWithTag.wait();
+      const { sendTransaction: sendTransactionWithTag } = await buildTransactionWithGasLimit(
+        stETH.approve,
+        [RaftConfig.getPositionManagerAddress('wstETH'), 1n],
+        undefined,
+        tag,
+        signer,
+      );
+      const transactionResponseWithTag = await sendTransactionWithTag();
+      await transactionResponseWithTag.wait();
 
-    const encodedTag = tag
-      .split('')
-      .map(c => c.charCodeAt(0).toString(16))
-      .join('');
+      const encodedTag = tag
+        .split('')
+        .map(c => c.charCodeAt(0).toString(16))
+        .join('');
 
-    expect(transactionResponseWithTag.data).toEqual(transactionResponseNoTag.data + encodedTag);
+      expect(transactionResponseWithTag.data).toEqual(transactionResponseNoTag.data + encodedTag);
 
-    const { sendTransaction: sendTransactionEmptyTag } = await buildTransactionWithGasLimit(
-      stETH.approve,
-      [RaftConfig.getPositionManagerAddress('wstETH'), 1n],
-      undefined,
-      '',
-      signer,
-    );
-    const transactionResponseEmptyTag = await sendTransactionEmptyTag();
-    await transactionResponseEmptyTag.wait();
+      const { sendTransaction: sendTransactionEmptyTag } = await buildTransactionWithGasLimit(
+        stETH.approve,
+        [RaftConfig.getPositionManagerAddress('wstETH'), 1n],
+        undefined,
+        '',
+        signer,
+      );
+      const transactionResponseEmptyTag = await sendTransactionEmptyTag();
+      await transactionResponseEmptyTag.wait();
 
-    expect(transactionResponseEmptyTag.data).toEqual(transactionResponseNoTag.data);
-  }, 15_000);
+      expect(transactionResponseEmptyTag.data).toEqual(transactionResponseNoTag.data);
+    },
+    20_000,
+  );
 });
