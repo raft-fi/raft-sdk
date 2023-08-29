3fd601db508e6eeb7a3bd1ff2e9dbb7c0fe4ad7e
Lazar Mitic <lazar@tempus.finance>
2023-08-29 14:54:00 +0200

    Add savings transaction history (#204)

--- src/savings/index.ts
+++ src/savings/index.ts
@@ -1,2 +1,7 @@
-export { type ManageSavingsStep, UserSavings } from './user-savings';
+export {
+  type ManageSavingsStep,
+  type SavingsTransaction,
+  type SavingsTransactionType,
+  UserSavings,
+} from './user-savings';
 export { Savings } from './savings';
