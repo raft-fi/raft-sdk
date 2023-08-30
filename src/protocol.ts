0f595b00b4b79509a1c4e7173db4025383f33303
Vukašin Manojlović <iamvukasin@gmail.com>
2023-08-30 11:08:10 +0200

    Add more protocol tests

--- src/protocol.ts
+++ src/protocol.ts
@@ -2,7 +2,7 @@ import { request, gql } from 'graphql-request';
 import { Provider, Signer, TransactionResponse } from 'ethers';
 import { Decimal } from '@tempusfinance/decimal';
 import { RaftConfig } from './config';
-import { ERC20PermitRToken, PositionManager, WrappedCollateralToken } from './typechain';
+import { WrappedCollateralToken } from './typechain';
 import {
   CollateralToken,
   R_TOKEN,
@@ -40,9 +40,6 @@ export class Protocol {
   private static instance: Protocol;
 
   private provider: Provider;
-  private positionManager: PositionManager;
-  private rToken: ERC20PermitRToken;
-
   private _collateralSupply: Record<UnderlyingCollateralToken, Decimal | null> = {
     wstETH: null,
     wcrETH: null,
@@ -66,8 +63,6 @@ export class Protocol {
    */
   private constructor(provider: Provider) {
     this.provider = provider;
-    this.positionManager = getPositionManagerContract('base', RaftConfig.networkConfig.positionManager, this.provider);
-    this.rToken = getTokenContract(R_TOKEN, this.provider);
   }
 
   /**
@@ -104,7 +99,12 @@ export class Protocol {
       // TODO: Needs `getRedeemCollateralSteps` for more granular control
       const positionManagerAddress = RaftConfig.networkConfig.wrappedCollateralTokenPositionManagers[collateralToken];
       const positionManager = getPositionManagerContract('wrapped', positionManagerAddress, redeemer);
-      const rPermitSignature = await createPermitSignature(redeemer, debtAmount, positionManagerAddress, this.rToken);
+      const rPermitSignature = await createPermitSignature(
+        redeemer,
+        debtAmount,
+        positionManagerAddress,
+        getTokenContract(R_TOKEN, this.provider),
+      );
 
       const { sendTransaction } = await buildTransactionWithGasLimit(
         positionManager.redeemCollateral,
@@ -211,12 +211,14 @@ export class Protocol {
    * @returns Fetched borrowing rate.
    */
   async fetchBorrowingRate(): Promise<Record<UnderlyingCollateralToken, Decimal | null>> {
+    const positionManager = getPositionManagerContract('base', RaftConfig.networkConfig.positionManager, this.provider);
+
     await Promise.all(
       UNDERLYING_COLLATERAL_TOKENS.map(async collateralToken => {
         const collateralTokenAddress = RaftConfig.getTokenAddress(collateralToken);
 
         this._borrowingRate[collateralToken] = new Decimal(
-          await this.positionManager.getBorrowingRate(collateralTokenAddress),
+          await positionManager.getBorrowingRate(collateralTokenAddress),
           Decimal.PRECISION,
         );
       }),
@@ -247,8 +249,9 @@ export class Protocol {
     const redeemedFraction = collateralAmount.mul(collateralPrice).div(totalDebtSupply);
 
     const collateralTokenAddress = RaftConfig.getTokenAddress(collateralToken);
+    const positionManager = getPositionManagerContract('base', RaftConfig.networkConfig.positionManager, this.provider);
     const [collateralInfo, latestBlock] = await Promise.all([
-      this.positionManager.collateralInfo(collateralTokenAddress),
+      positionManager.collateralInfo(collateralTokenAddress),
       this.provider.getBlock('latest'),
     ]);
     if (!latestBlock) {
@@ -295,7 +298,8 @@ export class Protocol {
    * @returns Fetched flash mint fee.
    */
   public async fetchFlashMintFee(): Promise<Decimal> {
-    this._flashMintFee = new Decimal(await this.rToken.flashMintFeePercentage(), 0).div(FLASH_MINT_FEE_PERCENTAGE_BASE);
+    const rToken = getTokenContract(R_TOKEN, this.provider);
+    this._flashMintFee = new Decimal(await rToken.flashMintFeePercentage(), 0).div(FLASH_MINT_FEE_PERCENTAGE_BASE);
     return this._flashMintFee;
   }
 
