0bed304bfb6a14c3ed7891d874ca981f624e8a83
Vukašin Manojlović <iamvukasin@gmail.com>
2023-09-11 10:02:43 +0200

    Simplify fetching token price from price feeds (#217)

--- src/config/mainnet.ts
+++ src/config/mainnet.ts
@@ -1,12 +1,12 @@
 import { ZeroAddress } from 'ethers';
 import { Decimal } from '@tempusfinance/decimal';
 import { NetworkConfig, TokenConfig, UnderlyingTokens } from './types';
-import { getWstEthToStEthRate } from '../price';
 import { Token } from '../types';
+import { getWstEthToStEthRate } from '../price';
 
 const POSITION_MANAGER_ADDRESS = '0x5f59b322eb3e16a0c78846195af1f588b77403fc';
 const POSITION_MANAGER_STETH_ADDRESS = '0x839d6833cee34ffab6fa9057b39f02bd3091a1d6';
-const POSITION_MANAGER_RETH = '0x29f8abb4cab4bbb56f617d9a3c0f62d33758e74e';
+const POSITION_MANAGER_RETH_ADDRESS = '0x29f8abb4cab4bbb56f617d9a3c0f62d33758e74e';
 const ONE_INCH_ONE_STEP_LEVERAGE_STETH_ADDRESS = '0xB2Bf4De5a63B2225338CdFdBAd045EA62f158b67';
 
 const underlyingTokensConfig: UnderlyingTokens = {
@@ -14,27 +14,19 @@ const underlyingTokensConfig: UnderlyingTokens = {
     supportedCollateralTokens: {
       stETH: {
         positionManager: POSITION_MANAGER_STETH_ADDRESS,
-        underlyingCollateralRate: getWstEthToStEthRate,
-        underlyingTokenTicker: 'wstETH',
       },
       wstETH: {
         positionManager: POSITION_MANAGER_ADDRESS,
-        underlyingCollateralRate: Decimal.ONE,
-        underlyingTokenTicker: 'wstETH',
       },
     },
   },
   wcrETH: {
     supportedCollateralTokens: {
       rETH: {
-        positionManager: POSITION_MANAGER_RETH,
-        underlyingCollateralRate: Decimal.ONE,
-        underlyingTokenTicker: 'wcrETH',
+        positionManager: POSITION_MANAGER_RETH_ADDRESS,
       },
       wcrETH: {
         positionManager: POSITION_MANAGER_ADDRESS,
-        underlyingCollateralRate: Decimal.ONE,
-        underlyingTokenTicker: 'wcrETH',
       },
     },
   },
@@ -44,50 +36,46 @@ const tokensConfig: Record<Token, TokenConfig> = {
   ETH: {
     address: ZeroAddress,
     ticker: 'ETH',
-    hardcodedPrice: null,
-    priceFeedTicker: null,
-    subgraphPriceDataTicker: 'ETH',
+    priceFeed: {
+      ticker: 'ETH',
+      fallbackToken: 'wstETH',
+      getFallbackRate: getWstEthToStEthRate,
+    }, // TODO: will be replaced with WETH price feed
     supportsPermit: false,
   },
   stETH: {
     address: '0xae7ab96520de3a18e5e111b5eaab095312d7fe84',
-    hardcodedPrice: null,
-    priceFeedTicker: null,
-    subgraphPriceDataTicker: 'stETH',
-    supportsPermit: false,
     ticker: 'stETH',
+    supportsPermit: false,
+    priceFeed: {
+      ticker: 'stETH',
+      fallbackToken: 'wstETH',
+      getFallbackRate: getWstEthToStEthRate,
+    },
   },
   wstETH: {
     address: '0x7f39c581f595b53c5cb19bd0b3f8da6c935e2ca0',
-    hardcodedPrice: null,
-    priceFeedTicker: 'wstETH',
-    subgraphPriceDataTicker: null,
-    supportsPermit: true,
     ticker: 'wstETH',
+    supportsPermit: true,
+    priceFeed: 'wstETH',
   },
   rETH: {
     address: '0xae78736cd615f374d3085123a210448e74fc6393',
-    hardcodedPrice: null,
-    priceFeedTicker: 'wcrETH',
-    subgraphPriceDataTicker: null,
-    supportsPermit: false,
     ticker: 'rETH',
+    supportsPermit: false,
+    priceFeed: 'wcrETH',
   },
   wcrETH: {
     address: '0xb69e35fb4a157028b92f42655090b984609ae598',
-    hardcodedPrice: null,
-    priceFeedTicker: 'wcrETH',
-    subgraphPriceDataTicker: null,
-    supportsPermit: true,
     ticker: 'wcrETH',
+    supportsPermit: true,
+    priceFeed: 'wcrETH',
   },
   R: {
     address: '0x183015a9ba6ff60230fdeadc3f43b3d788b13e21',
-    hardcodedPrice: Decimal.ONE,
-    priceFeedTicker: null,
-    subgraphPriceDataTicker: null,
-    supportsPermit: true,
     ticker: 'R',
+    supportsPermit: true,
+    priceFeed: Decimal.ONE,
   },
 };
 
@@ -96,7 +84,7 @@ export const mainnetConfig: NetworkConfig = {
   positionManagerStEth: POSITION_MANAGER_STETH_ADDRESS,
   oneInchOneStepLeverageStEth: ONE_INCH_ONE_STEP_LEVERAGE_STETH_ADDRESS,
   wrappedCollateralTokenPositionManagers: {
-    wcrETH: POSITION_MANAGER_RETH,
+    wcrETH: POSITION_MANAGER_RETH_ADDRESS,
   },
   raftCollateralTokens: {
     wstETH: '0xa7820009f79687d39f51909a01e7fd4b4d0663f8',
---
+++ tests/price/feed.test.ts
@@ -0,0 +1,52 @@
+import { request } from 'graphql-request';
+import { JsonRpcProvider } from 'ethers';
+import { PriceFeed, TOKENS } from '../../src';
+import { Decimal } from '@tempusfinance/decimal';
+
+jest.mock('graphql-request', () => ({
+  ...jest.requireActual('graphql-request'),
+  request: jest.fn(),
+}));
+
+const describeWhen = (condition: boolean) => (condition ? describe : describe.skip);
+
+const forkProvider = new JsonRpcProvider('http://127.0.0.1:8545');
+
+describeWhen(process.env.CI === 'true')('PriceFeed', () => {
+  describe('getPrice', () => {
+    it('should return non-zero price for each token', async () => {
+      (request as jest.Mock).mockRejectedValue(new Error('Failed to fetch price'));
+
+      for (const token of TOKENS) {
+        const priceFeed = new PriceFeed(forkProvider);
+        const price = await priceFeed.getPrice(token);
+        expect(price.gt(0)).toBeTruthy();
+      }
+    });
+
+    it('should return the same price for the same token', async () => {
+      const priceFeed = new PriceFeed(forkProvider);
+      const price1 = await priceFeed.getPrice('wstETH');
+      const price2 = await priceFeed.getPrice('wstETH');
+
+      expect(price1).toEqual(price2);
+    });
+  });
+
+  describe('getUnderlyingCollateralRate', () => {
+    it('should return valid rate for wstETH:stETH', async () => {
+      const priceFeed = new PriceFeed(forkProvider);
+      const rate = await priceFeed.getUnderlyingCollateralRate('wstETH', 'stETH');
+
+      expect(rate.gt(1)).toBeTruthy();
+      expect(rate.lt(2)).toBeTruthy();
+    });
+
+    it('should return 1:1 rate for wcrETH:rETH', async () => {
+      const priceFeed = new PriceFeed(forkProvider);
+      const rate = await priceFeed.getUnderlyingCollateralRate('wcrETH', 'rETH');
+
+      expect(rate.equals(Decimal.ONE)).toBeTruthy();
+    });
+  });
+});
