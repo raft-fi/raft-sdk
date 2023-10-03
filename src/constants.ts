70163064bcb859deae3009fd071a823afe593b25
ShawTim <shawtim.liu@gmail.com>
2023-10-04 03:53:14 +0800

    calculate veRAFT amount

--- src/constants.ts
+++ src/constants.ts
@@ -4,6 +4,9 @@ import { SwapRouter, UnderlyingCollateralToken } from './types';
 // Time constants
 
 export const SECONDS_IN_MINUTE = 60;
+export const SECONDS_IN_HOUR = 60 * SECONDS_IN_MINUTE;
+export const SECONDS_IN_DAY = 24 * SECONDS_IN_HOUR;
+export const SECONDS_IN_WEEK = 7 * SECONDS_IN_DAY;
 export const SECONDS_PER_YEAR = 60 * 60 * 24 * 365.2425; // One Gregorian year has 365.2425 days
 
 // Protocol constants
