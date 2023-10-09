ee702cc48820612ae4f2498f16bf3e4672c722cf
ShawTim <shawtim.liu@gmail.com>
2023-10-09 18:43:27 +0800

    new staking APR calculation, more understandable and clearer

--- tests/token/raft.test.ts
+++ tests/token/raft.test.ts
@@ -1,7 +1,7 @@
 import { Decimal } from '@tempusfinance/decimal';
 import { Provider } from 'ethers';
 import { describe, expect, it, vi } from 'vitest';
-import { RaftToken } from '../../src/token';
+import { RaftToken, UserVeRaftBalance } from '../../src/token';
 
 describe('RaftToken', () => {
   const DUMMY_ADDRESS = '0x0';
@@ -9,9 +9,9 @@ describe('RaftToken', () => {
   const YEAR_IN_SEC = 365 * 24 * 60 * 60;
   const YEAR_IN_MS = YEAR_IN_SEC * 1000;
 
-  it('should return the correct estimated APR based on different input', async () => {
+  it('should return the correct estimated APR based on different input, no existing stake', async () => {
     const token = new RaftToken(DUMMY_ADDRESS, DUMMY_PROVIDER);
-    vi.spyOn(token, 'fetchVeRaftAvgTotalSupply').mockResolvedValue(new Decimal(34567890));
+    vi.spyOn(token, 'calculateTotalVeRaftAnnualShare').mockResolvedValue(new Decimal(34567890));
     vi.spyOn(token, 'getMaxVeLockPeriod').mockResolvedValue(2 * YEAR_IN_SEC);
     vi.spyOn(token, 'getAnnualGiveAway').mockReturnValue(new Decimal(33333333));
     vi.spyOn(Date, 'now').mockReturnValue(0);
@@ -20,22 +20,22 @@ describe('RaftToken', () => {
       {
         bptAmount: new Decimal(123456),
         unlockTime: new Date(2 * YEAR_IN_MS),
-        expected: '0.24014311025686594',
+        expected: '0.719470541732917037',
       },
       {
         bptAmount: new Decimal(123456),
         unlockTime: new Date(YEAR_IN_MS),
-        expected: '0.120178372396786514',
+        expected: '0.240037826434745426',
       },
       {
         bptAmount: new Decimal(123456),
         unlockTime: new Date(0.5 * YEAR_IN_MS),
-        expected: '0.060115926166118135',
+        expected: '0.060049495659548021',
       },
       {
         bptAmount: new Decimal(123456),
         unlockTime: new Date(0.25 * YEAR_IN_MS),
-        expected: '0.030064652538262179',
+        expected: '0.01501487844298804',
       },
       {
         bptAmount: new Decimal(123456),
@@ -45,22 +45,22 @@ describe('RaftToken', () => {
       {
         bptAmount: new Decimal(234567),
         unlockTime: new Date(2 * YEAR_IN_MS),
-        expected: '0.239759521505545377',
+        expected: '0.71774921205561505',
       },
       {
         bptAmount: new Decimal(234567),
         unlockTime: new Date(YEAR_IN_MS),
-        expected: '0.120082227790997352',
+        expected: '0.239845919647295439',
       },
       {
         bptAmount: new Decimal(234567),
         unlockTime: new Date(0.5 * YEAR_IN_MS),
-        expected: '0.060091858994736769',
+        expected: '0.060037478270475757',
       },
       {
         bptAmount: new Decimal(234567),
         unlockTime: new Date(0.25 * YEAR_IN_MS),
-        expected: '0.030058631862113471',
+        expected: '0.015014126992759619',
       },
       {
         bptAmount: new Decimal(234567),
@@ -71,8 +71,78 @@ describe('RaftToken', () => {
 
     for (const { bptAmount, unlockTime, expected } of tests) {
       const result = await token.estimateStakingApr(bptAmount, unlockTime, {
-        bptLockedBalance: Decimal.ZERO,
-        veRaftBalance: Decimal.ZERO,
+        userVeRaftBalance: { veRaftBalance: Decimal.ZERO } as UserVeRaftBalance,
+      });
+      expect(result.toString().substring(0, 16)).toBe(expected.substring(0, 16));
+    }
+  });
+
+  it('should return the correct estimated APR based on different input, with existing stake', async () => {
+    const token = new RaftToken(DUMMY_ADDRESS, DUMMY_PROVIDER);
+    vi.spyOn(token, 'calculateTotalVeRaftAnnualShare').mockResolvedValue(new Decimal(34567890));
+    vi.spyOn(token, 'getMaxVeLockPeriod').mockResolvedValue(2 * YEAR_IN_SEC);
+    vi.spyOn(token, 'getAnnualGiveAway').mockReturnValue(new Decimal(33333333));
+    vi.spyOn(Date, 'now').mockReturnValue(0);
+
+    const tests = [
+      {
+        bptAmount: new Decimal(123456),
+        unlockTime: new Date(2 * YEAR_IN_MS),
+        expected: '0.721550941900645252',
+      },
+      {
+        bptAmount: new Decimal(123456),
+        unlockTime: new Date(YEAR_IN_MS),
+        expected: '0.240733152195006668',
+      },
+      {
+        bptAmount: new Decimal(123456),
+        unlockTime: new Date(0.5 * YEAR_IN_MS),
+        expected: '0.060223559478386645',
+      },
+      {
+        bptAmount: new Decimal(123456),
+        unlockTime: new Date(0.25 * YEAR_IN_MS),
+        expected: '0.015058408939572996',
+      },
+      {
+        bptAmount: new Decimal(123456),
+        unlockTime: new Date(0),
+        expected: '0',
+      },
+      {
+        bptAmount: new Decimal(234567),
+        unlockTime: new Date(2 * YEAR_IN_MS),
+        expected: '0.719819655114133613',
+      },
+      {
+        bptAmount: new Decimal(234567),
+        unlockTime: new Date(YEAR_IN_MS),
+        expected: '0.240540132438391967',
+      },
+      {
+        bptAmount: new Decimal(234567),
+        unlockTime: new Date(0.5 * YEAR_IN_MS),
+        expected: '0.060211472326403206',
+      },
+      {
+        bptAmount: new Decimal(234567),
+        unlockTime: new Date(0.25 * YEAR_IN_MS),
+        expected: '0.015057653125993203',
+      },
+      {
+        bptAmount: new Decimal(234567),
+        unlockTime: new Date(0),
+        expected: '0',
+      },
+    ];
+
+    for (const { bptAmount, unlockTime, expected } of tests) {
+      const result = await token.estimateStakingApr(bptAmount, unlockTime, {
+        userVeRaftBalance: {
+          veRaftBalance: new Decimal(200000),
+          unlockTime: new Date(YEAR_IN_MS),
+        } as UserVeRaftBalance,
       });
       expect(result.toString().substring(0, 16)).toBe(expected.substring(0, 16));
     }
